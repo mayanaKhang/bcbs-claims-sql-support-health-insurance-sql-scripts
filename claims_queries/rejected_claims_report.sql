696d3a28870a45bfa1b0718b92db0fbfd58c00ec
KhanMayanaG <khan.gm2025@gmail.com>
2025-06-25 23:06:19 +0530

    Create rejected_claims_report.sql

---
+++ claims_queries/rejected_claims_report.sql
@@ -0,0 +1,8 @@
+-- Script: rejected_claims_report.sql
+-- Purpose: Generate a report of rejected claims with reasons
+-- Author: [Your Name]
+
+SELECT claim_id, member_id, rejection_reason, claim_date, amount
+FROM claims
+WHERE status = 'Rejected'
+ORDER BY claim_date DESC;
