d0f542ac5cfea530e0983cb0741a5d8c9d61bf81
KhanMayanaG <khan.gm2025@gmail.com>
2025-06-25 23:18:02 +0530

    Delete policy_queries/claims_queries directory

--- policy_queries/claims_queries/rejected_claims_report.sql
+++
@@ -1,8 +0,0 @@
--- Script: rejected_claims_report.sql
--- Purpose: Generate a report of rejected claims with reasons
--- Author: khan
-
-SELECT claim_id, member_id, rejection_reason, claim_date, amount
-FROM claims
-WHERE status = 'Rejected'
-ORDER BY claim_date DESC;
